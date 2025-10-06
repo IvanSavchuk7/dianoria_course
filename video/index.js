3726f45cf2b1c4a79d1a3625e9a0d5e89b82c2ab
Ivan Savchuk <116903781+IvanSavchuk7@users.noreply.github.com>
2025-10-06 23:12:12 +0300

    Add files via upload

---
+++ video/index.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("header-form");
+
+    const urlParams = new URLSearchParams(window.location.search);
+    ["utm_source", "utm_campaign", "utm_content", "utm_term"].forEach(param => {
+        const value = urlParams.get(param);
+        if (value) {
+            const hiddenInput = document.querySelector(`input[name="${param}"]`);
+            if (hiddenInput) hiddenInput.value = value;
+        }
+    });
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const submitBtn = document.getElementById("submit-btn-1");
+        submitBtn.disabled = true;
+
+        const formData = new FormData(form);
+
+        fetch("https://script.google.com/macros/s/AKfycbxk-8uFdbovzD4Z9sh9OAi3lhIyVNLqL8QayUPWNDM4SxPUuBsGaCWrcTGd-AS4RQBi/exec", {
+            method: "POST",
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.result === "success") {
+                    setTimeout(() => {
+                        window.location.href = "https://google.com/";
+                    }, 10);
+                }
+            });
+    });
+});
+
+document.body.classList.add('loading');
+
+window.onload = function () {
+    document.body.style.opacity = '1';
+    document.body.classList.remove('loading');
+};
