f605a253872edf5d24842db2e2101feadb9664c9
Ivan Savchuk <116903781+IvanSavchuk7@users.noreply.github.com>
2025-10-17 14:44:32 +0300

    Add files via upload

---
+++ new_landing_2/index.js
@@ -0,0 +1,104 @@
+var swiper1 = new Swiper(".feedback-swiper", {
+    slidesPerView: 1,
+    lazy: true,
+    autoHeight: false,
+    spaceBetween: 20,
+    mousewheel: false,
+    direction: 'horizontal',
+    loop: true,
+
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        601: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1080: {
+            slidesPerView: 2,
+            spaceBetween: 25,
+        },
+        1560: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            mousewheel: false,
+        },
+    }
+});
+var swiper2 = new Swiper(".results-swiper", {
+    slidesPerView: 1,
+    lazy: true,
+    autoHeight: false,
+    spaceBetween: 20,
+    mousewheel: false,
+    direction: 'horizontal',
+    loop: true,
+
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        420: {
+            slidesPerView: 1.25,
+            spaceBetween: 20,
+        },
+        601: {
+            slidesPerView: 1.35,
+            spaceBetween: 20,
+        },
+        1080: {
+            slidesPerView: 2,
+            spaceBetween: 25,
+        },
+        1560: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            mousewheel: false,
+        },
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    const form = document.getElementById("header-form");
+
+    const urlParams = new URLSearchParams(window.location.search);
+    ["utm_source", "utm_campaign", "utm_content", "utm_term"].forEach(param => {
+        const value = urlParams.get(param);
+        if (value) {
+            const hiddenInput = document.querySelector(`input[name="${param}"]`);
+            if (hiddenInput) hiddenInput.value = value;
+        }
+    });
+
+    form.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const submitBtn = document.getElementById("submit-btn-1");
+        submitBtn.disabled = true;
+
+        const formData = new FormData(form);
+
+        fetch("https://script.google.com/macros/s/AKfycbxk-8uFdbovzD4Z9sh9OAi3lhIyVNLqL8QayUPWNDM4SxPUuBsGaCWrcTGd-AS4RQBi/exec", {
+            method: "POST",
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.result === "success") {
+                    setTimeout(() => {
+                        window.location.href = "https://google.com";
+                    }, 10);
+                }
+            });
+    });
+});
