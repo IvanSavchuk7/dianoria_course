04a88cad578e80fc915323902f63d8e0ac59270f
Ivan Savchuk <116903781+IvanSavchuk7@users.noreply.github.com>
2025-12-26 16:12:21 +0200

    Add files via upload

--- anketav3/index.js
+++ anketav3/index.js
@@ -1,3 +1,37 @@
+var swiper1 = new Swiper(".feedback-swiper", {
+    slidesPerView: 1,
+    lazy: true,
+    autoHeight: false,
+    spaceBetween: 20,
+    mousewheel: false,
+    direction: 'horizontal',
+    loop: true,
+
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        601: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1080: {
+            slidesPerView: 2,
+            spaceBetween: 25,
+        },
+        1560: {
+            slidesPerView: 3,
+            spaceBetween: 25,
+            mousewheel: false,
+        },
+    }
+});
+
 function isObviouslyFakeNumber(num) {
     const digits = num.replace(/\D/g, "");
     if (/^(\d)\1{5,}$/.test(digits)) return true;
